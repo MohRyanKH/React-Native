2d69b9539f8fb4f0a153e3ca1704381e3a9e5870
MohRyanKH <rykhalda@gmail.com>
2023-01-15 17:24:23 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <View>
+          <Text>App</Text>
+        </View>
+        <Text>Halo Kawan</Text>
+        <Text>Baik Belajar Dulu Sebelum Build atau Membangun UI maupun Logic</Text>
+        <Text>Untuk Waktu React JS satu bulan dan React Native satu Bulan</Text>
+        <Text>Total 2 Bulan</Text>
+        <Text>Sehari dapat jatah belajar 2 materi per react</Text>
+        <Text>yang di kedepankan react native nya dan pembuatan website Balai Desa</Text>
+      </View>
+    )
+  }
+}
